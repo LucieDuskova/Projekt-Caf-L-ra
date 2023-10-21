54f6002303ba817f8c1bd11f7e1788eeb17f7385
Lucie Dušková <lucie.duskova87@gmail.com>
2023-10-21 10:47:10 +0200

    Komponenty2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,114 +1,20 @@
 import { render } from '@czechitas/render';
 import { Header } from '../components/01_Header/Header.jsx';
 import { Footer } from '../components/06_Footer/footer.jsx';
+import { Banner } from '../components/02_Banner/Banner.jsx';
+import { Menu } from '../components/03_Menu/menu.jsx';
 import '../global.css';
 import './index.css';
+
+
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
-    {/* <header>
-      <div className="header__content container">
-        <div className="site-logo"></div>
 
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
-        </div>
-
-      </div>
-    </header> */}
     <main>
-      <section className="banner">
-        <div className="container">
-          <div className="banner__content">
-            <h1>Přijď to ochutnat!</h1>
-            <p>Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.</p>
-          </div>
-        </div>
-      </section>
-      <section className="menu">
-        <div className="container">
-          <h2>Naše nabídka</h2>
-          <p className="menu-intro">
-            Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
-          </p>
-          <div className="drinks-list">
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/espresso.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Espresso</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/doppio.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Doppio</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/lungo.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Lungo</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#b0dee1' }} />
-                    <div className="layer__label">voda</div>
-                  </div>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <div className="drink__controls">
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="order-detail">
-            <a href="/objednavka">Detail objednávky</a>
-          </div>
-        </div>
-      </section>
+    <Banner />
+    <Menu/>
 
       <section className="gallery">
         <div className="container">
@@ -160,12 +66,5 @@ document.querySelector('#root').innerHTML = render(
     </main>
 
     <Footer />
-    {/* <footer>
-      <div className="container">
-        <div className="footer__content">
-          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-        </div>
-      </div>
-    </footer> */}
   </div>
 );
